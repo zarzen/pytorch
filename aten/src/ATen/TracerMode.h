fd32357a3b5a58b8a48c011f284931ae5f5b3d14
Taylor Robie <taylorrobie@fb.com>
2021-06-09 09:14:22 -0700

    move local dispatch key storage to a TLS seperate from the LocalDispatchKeySet.h
    ghstack-source-id: 586fd3b538883319ed97bd6d8e6021d3951d5bc5
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/59391

--- aten/src/ATen/TracerMode.h
+++ aten/src/ATen/TracerMode.h
@@ -125,9 +125,8 @@ static inline void set_dispatch_enabled(bool enabled) {
   c10::impl::tls_set_dispatch_key_included(at::DispatchKey::Tracer, enabled);
 }
 
-struct NoTracerDispatchMode {
-  c10::impl::ExcludeDispatchKeyGuard guard_{at::DispatchKey::Tracer};
-};
+using NoTracerDispatchMode = c10::impl::ExcludeSingleDispatchKeyGuard<
+  at::DispatchKey::Tracer, /*has_overlap=*/false>;
 
 } // namespace impl
 } // namespace tracer
