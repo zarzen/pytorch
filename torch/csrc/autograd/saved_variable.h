6834df9ca82bbdb4c81bb110b221923b15fe7ba0
Victor Quach <quach@fb.com>
2021-06-10 19:40:20 -0400

    Remove dead code in SavedVariable
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 59411abf9c65fc92a4f2d98bc2da5d5756a8eb2a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/59838

--- torch/csrc/autograd/saved_variable.h
+++ torch/csrc/autograd/saved_variable.h
@@ -42,7 +42,7 @@ class TORCH_API SavedVariable {
   }
 
   void reset_grad_function() {
-    grad_fn_.reset();
+    // No op
   }
 
  private:
@@ -62,15 +62,9 @@ class TORCH_API SavedVariable {
   // either the saved Tensor or the unpacked Tensor. See note [ Using ForwardGrad ]
   std::shared_ptr<ForwardGrad> fw_grad_;
 
-  // The gradient function associated with this node. If has_grad_fn
-  // is false, then this is a leaf node. Note that the grad_fn is not saved if
-  // it would create a circular reference. In that case, the grad_fn must be
-  // passed in to the unpack function when reconstructing the Variable.
-  std::shared_ptr<Node> grad_fn_;
   // Weak version of grad_fn_ that prevents leaks in rebase_history() for
   // inplace views.
   std::weak_ptr<Node> weak_grad_fn_;
-  std::weak_ptr<Node> grad_accumulator_;
   c10::VariableVersion version_counter_;
 
   uint32_t saved_version_ = 0;
