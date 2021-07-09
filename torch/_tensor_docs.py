0dfc7dca85d660cdcc239b9fb0d0f384f3428ec3
Peter Bell <peterbell10@live.co.uk>
2021-07-09 13:24:03 +0100

    Give std/var correction overloads proper defaults
    ghstack-source-id: 66b0342227e60b723cbe193522c657707bf7dbc7
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/56398

--- torch/_tensor_docs.py
+++ torch/_tensor_docs.py
@@ -3616,12 +3616,7 @@ In-place version of :meth:`~Tensor.squeeze`
 
 add_docstr_all('std',
                r"""
-std(dim, unbiased=True, keepdim=False) -> Tensor
-
-See :func:`torch.std`
-
-.. function:: std(unbiased=True) -> Tensor
-   :noindex:
+std(dim=None, *, correction=1, keepdim=False) -> Tensor
 
 See :func:`torch.std`
 """)
@@ -4298,12 +4293,7 @@ In-place version of :meth:`~Tensor.unsqueeze`
 
 add_docstr_all('var',
                r"""
-var(dim, unbiased=True, keepdim=False) -> Tensor
-
-See :func:`torch.var`
-
-.. function:: var(unbiased=True) -> Tensor
-   :noindex:
+var(dim=None, *, correction=1, keepdim=False) -> Tensor
 
 See :func:`torch.var`
 """)
