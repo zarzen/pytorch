aca02eda348dfec230c228b66f5430d2b028e5f3
Meghan Lele <meghanl@fb.com>
2021-07-13 17:34:39 -0700

    [WIP] `nll_loss_forward`: port to structured kernel
    For more information, see #55070.
    
    ghstack-source-id: 20cc1bce8e7cfd24a1efb17b315fe1ea2ebb117b
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/61443

--- aten/src/ATen/core/Tensor.h
+++ aten/src/ATen/core/Tensor.h
@@ -1,3 +1,36 @@
 #pragma once
 
 #include <ATen/core/TensorBody.h>
+#include <c10/util/Exception.h>
+
+namespace at {
+class TORCH_API OptionalTensorRef {
+ public:
+  OptionalTensorRef() {}
+  OptionalTensorRef(const Tensor& src)
+      : ref_(c10::intrusive_ptr<TensorImpl>(
+            src.unsafeGetTensorImpl(),
+            c10::raw::DontIncreaseRefcount{})) {
+    TORCH_INTERNAL_ASSERT(src.defined());
+  }
+
+  ~OptionalTensorRef() {
+    ref_.unsafeReleaseTensorImpl();
+  }
+
+  bool has_value() const {
+    return ref_.defined();
+  }
+
+  const Tensor& getTensorRef() const {
+    return ref_;
+  }
+
+  operator bool() const {
+    return ref_.defined();
+  }
+
+ private:
+  Tensor ref_;
+};
+} // namespace at
