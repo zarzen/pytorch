f82d43fdc6a3a358e87279682ff91f3d5b69a43b
Basil Hosmer <bhosmer@fb.com>
2021-07-15 18:51:57 +0000

    preserve residual in transformer norm_first
    ghstack-source-id: 221c5656f9164ceda5331be74e7a50ecabaffd3d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/61692

--- torch/nn/modules/transformer.py
+++ torch/nn/modules/transformer.py
@@ -321,25 +321,31 @@ class TransformerEncoderLayer(Module):
         Shape:
             see the docs in Transformer class.
         """
-        if self.norm_first:
-            src = self.norm1(src)
-            src2 = self.self_attn(src, src, src, attn_mask=src_mask,
-                                  key_padding_mask=src_key_padding_mask)[0]
-            src = src + self.dropout1(src2)
-            src = self.norm2(src)
-            src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
-            src = src + self.dropout2(src2)
-            return src
 
-        # norm last
-        src2 = self.self_attn(src, src, src, attn_mask=src_mask,
-                              key_padding_mask=src_key_padding_mask)[0]
-        src = src + self.dropout1(src2)
-        src = self.norm1(src)
-        src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
-        src = src + self.dropout2(src2)
-        src = self.norm2(src)
-        return src
+        # see Fig. 1 of https://arxiv.org/pdf/2002.04745v1.pdf
+
+        # self-attention block
+        def mha(x):
+            x = self.self_attn(x, x, x,
+                               attn_mask=src_mask,
+                               key_padding_mask=src_key_padding_mask,
+                               need_weights=False)[0]
+            return self.dropout1(x)
+
+        # feed forward block
+        def ff(x):
+            x = self.linear2(self.dropout(self.activation(self.linear1(x))))
+            return self.dropout2(x)
+
+        x = src
+        if self.norm_first:
+            x = x + mha(self.norm1(x))
+            x = x + ff(self.norm2(x))
+        else:
+            x = self.norm1(x + mha(x))
+            x = self.norm2(x + ff(x))
+
+        return x
 
 
 class TransformerDecoderLayer(Module):
